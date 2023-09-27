1b4191edabf21b53e387abe06b6c5c0c8f40e71b
unifusions <siyam@unifusions.com>
2023-09-28 00:11:10 +0530

    main branch

--- app/Http/Controllers/Admin/ARNController.php
+++ app/Http/Controllers/Admin/ARNController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ARNController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. in laptop
      */
     public function index()
     {
