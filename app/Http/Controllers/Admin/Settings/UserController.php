149296bf6dfe6886f9b2802a3f87e5e840bc0204
ranjaniavnl <ranjanimanickam@gmail.com>
2023-10-30 13:09:26 +0530

    user

--- app/Http/Controllers/Admin/Settings/UserController.php
+++ app/Http/Controllers/Admin/Settings/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
 {
     // public function __construct(){
     //     $this->authorizeResource(User::class, 'user');
-    // }fsfshfsuifhs
+    // }fsfshfsuifhs fgdfgedgd
 
     public function index()
     {
