fc564ddbabd4baf39740e11c797beb86af5502da
ranjaniavnl <ranjanimanickam@gmail.com>
2023-10-30 12:45:47 +0530

    user

---
+++ app/Http/Controllers/Admin/Settings/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Settings;
+
+use App\Http\Controllers\Controller;
+// use App\Models\Division;
+// use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class UserController extends Controller
+{
+    // public function __construct(){
+    //     $this->authorizeResource(User::class, 'user');
+    // }
+    public function index()
+    {
+       
+        return view('admin.settings.users.index',
+         ['users'=> User::paginate(10)]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // $divisions = Division::all();
+        // $permissionsList = Permission::orderBy('name', 'ASC')->get()->groupBy('model');
+        return view('admin.settings.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         // ]);
+         $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+           
+        ]);
+       
+        return redirect()->route('user.index')->with('success', 'User ' . $user->name . ' has been created succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('admin.settings.users.edit', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        User::where('id',$id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+
+        ]);
+        $user= User::where('id',$id)->first();
+               return redirect()->route('user.index')->with('success', 'User ' . $user->name . ' has been modified succesfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('user.index')->with([
+            'status' => 'success',
+            'message' => 'User - <b>' . $user->name .  '</b> has been deleted Successfully',
+        ]);
+    }
+}
--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -71,6 +71,48 @@
                     </x-card>
                 </a>
             </div>
+            <div class="col">
+                <a href="{{ route('user.index') }}" class="text-decoration-none">
+                    <x-card class="border-0 " style="box-shadow: 0 0.15rem 0.1rem rgba(0, 0, 0, 0.04);">
+
+                        <div class="d-flex align-items-center">
+                            <div class="p-3 me-3" style="background-color:#E7F9F9">
+                                <svg viewBox="0 -5.5 64 64" height="30" fill="#12C2C2">
+                                    <g>                                        
+                                        <g stroke-width="1" fill="none"
+                                            fill-rule="evenodd" sketch:type="MSPage">
+                                            <g id="Factory" sketch:type="MSLayerGroup"
+                                                transform="translate(1.000000, 1.000000)" stroke="#12C2C2"
+                                                stroke-width="2">
+                                                <path
+                                                    d="M60,20 L56,20 L56,0 L47,0 L47,27.5 L34,20.6 L34,27.5 L22,20.6 L22,27.5 L11,20.6 L11,27.5 L2,20.6 C0.9,20.6 0,21.5 0,22.6 L0,48.1 C0,49.2 0.9,50.1 2,50.1 L60,50.1 C61.1,50.1 62,49.2 62,48.1 L62,22.1 C62,20.9 61.1,20 60,20 L60,20 Z"
+                                                    id="Shape" sketch:type="MSShapeGroup"> </path>
+                                                <rect id="Rectangle-path" sketch:type="MSShapeGroup" x="4" y="32"
+                                                    width="4" height="4"> </rect>
+                                                <rect id="Rectangle-path" sketch:type="MSShapeGroup" x="14" y="32"
+                                                    width="4" height="4"> </rect>
+                                                <rect id="Rectangle-path" sketch:type="MSShapeGroup" x="25" y="32"
+                                                    width="4" height="4"> </rect>
+                                                <rect id="Rectangle-path" sketch:type="MSShapeGroup" x="37" y="32"
+                                                    width="4" height="4"> </rect>
+                                                <path d="M0,46.1 L62,46.1" id="Shape" sketch:type="MSShapeGroup">
+                                                </path>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </div>
+                            <div>
+                                <h5 class="fs-20 mb-0 fw-bold">User</h5>
+
+
+                            </div>
+                        </div>
+
+                        
+                    </x-card>
+                </a>
+            </div>
         </div>
 
 
